eb90f543ecf25f4c9405763ea7b24d178c6fa155
Davee Sok <daveesok@gmail.com>
2020-06-09 17:35:02 -0700

    updated

--- validation.js
+++ validation.js
@@ -3,12 +3,20 @@
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
-  telephone: /^\d{11}$/
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8-20}$/,
+  slug: /^[a-z\d-]{8-20}$/,
+  email: /^([a-z\d\.-]+)@(a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 
 function validate(field, regex) {
-  console.log(regex.test(field.value));
+  if (regex.test(field.value)) {
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
 
 }
 
